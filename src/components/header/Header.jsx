17eb50c0b69fdd75a332e30444508221f0453e84
Rupa Mahindrakar <rupamahindrakar08@gmail.com>
2024-07-01 16:34:56 +0530

    All Post functionality changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -37,35 +37,6 @@ function Header() {
 
 
   return (
-    // <header className='py-3 shadow'>
-    //   <Container>
-    //     <nav className='flex'>
-    //       <div className='mr-4'>
-    //         <Link to='/'>
-    //           <Logo width='50px'   />
-    //         </Link>
-    //       </div>
-    //       <ul className='flex ml-auto'>
-    //         {navItems.map((item) => 
-    //         item.active ? (
-    //           <li key={item.name}>
-    //             <button
-    //             onClick={() => navigate(item.slug)}
-    //             className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    //             >{item.name}</button>
-    //           </li>
-    //         ) : null
-    //         )}
-    //         {authStatus && (
-    //           <li>
-    //             <LogoutBtn />
-    //           </li>
-    //         )}
-    //       </ul>
-    //     </nav>
-    //     </Container>
-    // </header>
-
     <header className="py-3 shadow">
       <Container>
         <nav className="flex items-center justify-between">
--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -10,19 +10,26 @@ function AllPosts() {
             setPosts(posts.documents)
         }
     })
+
     return (
         <div className='w-full min-h-screen py-8'>
             <Container>
-                <div className='flex flex-wrap '>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
+                {posts.length === 0 ? (
+                    <div className='text-center inline-block'>
+                        <p className='text-2xl mt-5 shadow-md p-4 rounded-md'>No Posts Available</p>
+                    </div>
+                ) : (
+                    <div className='flex flex-wrap'>
+                        {posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4'>
+                                <PostCard {...post} />
+                            </div>
+                        ))}
+                    </div>
+                )}
             </Container>
         </div>
-    )
+    );
 }
 
 export default AllPosts
